3bb48b78129623349af409eb9ca0184d1b57ae0e
maccchan4869 <ymatsuo554@gmail.com>
2020-11-19 17:36:48 +0900

    レビュー後修正

--- task1-3.js
+++ task1-3.js
@@ -1,39 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const MESSAGE = {
-    INPUT: "タスクを入力してください",
-    SUCCESS: "新しいタスクを追加しました"
-  };
-  
-  const HEADER = {
-    TEXT: "現在持っているタスク一覧\n",
-    FRAME: "========================\n"
-  };
-  
-  const SEPARATION = " ： ";
-  
-  // タスクの表示処理
-  const DispTask = function () {
-    console.log(HEADER.FRAME + HEADER.TEXT + HEADER.FRAME);
-    for (let i = 0; i < taskArray.length; i++) {
-      console.log(i + SEPARATION + taskArray[i]);
-    }
-  };
+const taskArray = [];
 
-  const AddTask = function () {
-    let input = window.prompt(MESSAGE.INPUT);
-    taskArray.push(input);
-    DispTask();
-    alert(MESSAGE.SUCCESS); 
-  };
-  
-  let taskArray = [];
-  
-  // 初期表示
-  taskArray.push("掃除");
-  taskArray.push("買い物");
-  taskArray.push("散歩");
-  DispTask();
-  
-  // タスクの追加処理
-  setTimeout(AddTask, 5000);
-});
+const separation = ' ： ';
+
+const message = {
+  input: 'タスクを入力してください',
+  success: '新しいタスクを追加しました'
+};
+ 
+const header = {
+  text: '現在持っているタスク一覧\n',
+  frame: '========================\n'
+};
+
+// タスクの表示処理
+const dispTask = () => {
+  console.log(header.frame + header.text + header.frame);
+  taskArray.forEach((value, index) => console.log(index + separation + value));
+};
+
+// タスクの追加処理
+const addTask = () => {
+  const inputTask = window.prompt(message.input);
+  taskArray.push(inputTask);
+  dispTask();
+  alert(message.success); 
+};
+
+// 初期表示
+taskArray.push('掃除');
+taskArray.push('買い物');
+taskArray.push('散歩');
+dispTask();
+
+// 追加表示
+setTimeout(addTask, 5000);
